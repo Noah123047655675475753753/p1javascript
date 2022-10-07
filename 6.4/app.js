181192381d523683efbe12c2e2bae79bbdba951a
Noah123047655675475753753 <9018300@student.zadkine.nl>
2022-10-07 09:40:29 +0200

    6.4

---
+++ 6.4/app.js
@@ -0,0 +1,19 @@
+function calculate(x, y) {
+    let anwer= x + y;
+    console.log(anwer);
+}
+
+calculate(1,12);
+
+function multiplyBYFive(x) {
+let anwer= num * 5;
+console.log(anwer)
+}
+
+calculate(50)
+
+function calulateMinutes(x) {
+    console.log(x / 60)
+}
+
+calculate(3600)
