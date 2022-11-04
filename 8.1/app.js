801e7b30f979a95a2ce87c07701cdc3b9fc55c55
Noah123047655675475753753 <9018300@student.zadkine.nl>
2022-11-04 10:49:07 +0100

    8.2

--- 8.1/app.js
+++ 8.1/app.js
@@ -5,27 +5,35 @@ let input2 = document.getElementById("input2")
 
 
 function add() {
+    if(input1.value > 0 && input2.value > 0) {
  let answer =  Number(input1.value) + Number(input2.value)
 
  title.innerText = "Het antwoord = " + answer;
+    }
 }
 
 function mulitply() {
+    if(input1.value > 0 && input2.value > 0) {
     let answer = input1.value * input2.value
 
  title.innerText = "Het antwoord = " + answer;
 }
+}
 
 function devide() {
+    if(input1.value > 0 && input2.value > 0) {
     let answer = input1.value / input2.value
 
    title.innerText = "Het antwoord = " + answer;
 }
+}
 
 function minus() {
+    if(input1.value > 0 && input2.value > 0) {
     let answer = input1.value - input2.value
 
     title.innerText = "Het antwoord = " + answer;
+} 
 }
 
 
